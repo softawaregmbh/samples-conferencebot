882d5090e653f4ef0091c70bec7724ae0c5e32b7
Roman Schacherl <roman.schacherl@softaware.at>
2019-03-08 09:57:42 +0100

    Conference bot with waterfall dialog

--- ConferenceBot/ConferenceBot/ConferenceBotBot.cs
+++ ConferenceBot/ConferenceBot/ConferenceBotBot.cs
@@ -1,9 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
 using Microsoft.Bot.Schema;
 
 namespace ConferenceBot
@@ -12,46 +17,82 @@ namespace ConferenceBot
     {
         private readonly ConversationState conversationState;
         private IStatePropertyAccessor<ScheduleQuery> scheduleQueryAccessor;
+        private IStatePropertyAccessor<DialogState> dialogStateAccessor;
+        private DialogSet dialogs;
 
         public ConferenceBotBot(ConversationState conversationState)
         {
+            this.dialogStateAccessor = conversationState.CreateProperty<DialogState>(nameof(DialogState));
             this.scheduleQueryAccessor = conversationState.CreateProperty<ScheduleQuery>(nameof(ScheduleQuery));
             this.conversationState = conversationState ?? throw new System.ArgumentNullException(nameof(conversationState));
+
+            this.dialogs = new DialogSet(this.dialogStateAccessor);
+
+            this.dialogs.Add(new DateTimePrompt("time"));
+            this.dialogs.Add(new ChoicePrompt("track")
+            {
+                Style = ListStyle.SuggestedAction
+            });
+
+            this.dialogs.Add(new WaterfallDialog("scheduleDialog")
+                        .AddStep(TimeStepAsync)
+                        .AddStep(TrackStepAsync)
+                        .AddStep(CompleteStepAsync));
         }
-        
+
         public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
         {
             if (turnContext.Activity.Type == ActivityTypes.Message)
             {
-                var scheduleQuery = await scheduleQueryAccessor.GetAsync(turnContext, () => new ScheduleQuery());
+                var dialogContext = await dialogs.CreateContextAsync(turnContext);
+                var result = await dialogContext.ContinueDialogAsync();
 
-                var message = turnContext.Activity.Text;
-
-                switch (scheduleQuery.LastQuestion)
+                if (result.Status == DialogTurnStatus.Empty)
                 {
-                    case QuestionType.Time:
-                        scheduleQuery.Time = message;
-                        break;
-                    case QuestionType.Track:
-                        scheduleQuery.Track = message;
-                        break;
-                    default:
-                        break;
-                }
-
-                if (scheduleQuery.Time == null)
-                {
-                    scheduleQuery.LastQuestion = QuestionType.Time;
-                    await turnContext.SendActivityAsync("Um welche Uhrzeit?");
-                }
-                else if (scheduleQuery.Track == null)
-                {
-                    scheduleQuery.LastQuestion = QuestionType.Track;
-                    await turnContext.SendActivityAsync("In welchem Track?");
+                    await dialogContext.BeginDialogAsync("scheduleDialog");
                 }
 
                 await conversationState.SaveChangesAsync(turnContext);
             }
+        }
+        private async Task<DialogTurnResult> TimeStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.PromptAsync("time", new PromptOptions
+            {
+                Prompt = MessageFactory.Text("Um welche Uhrzeit?")
+            });
+        }
+
+        private async Task<DialogTurnResult> TrackStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var scheduleQuery = await scheduleQueryAccessor.GetAsync(stepContext.Context, () => new ScheduleQuery());
+
+            var dateTimes = stepContext.Result as IList<DateTimeResolution>;
+            scheduleQuery.Time = dateTimes?.FirstOrDefault()?.Value;
+
+            return await stepContext.PromptAsync("track", new PromptOptions
+            {
+                Prompt = MessageFactory.Text("Auf welchem Track?"),
+                Choices = new []
+                {
+                    new Choice("Apps & Infrastructure"),
+                    new Choice("Data & AI"),
+                    new Choice("Modern Workplace"),
+                    new Choice("Hands-on Sessions")
+                }
+            });
+        }
+
+        private async Task<DialogTurnResult> CompleteStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var scheduleQuery = await scheduleQueryAccessor.GetAsync(stepContext.Context);
+            scheduleQuery.Track = ((FoundChoice)stepContext.Result).Value;
+
+            await stepContext.Context.SendActivityAsync($"Wir sehen nach, was um {scheduleQuery.Time} im {scheduleQuery.Track}-Track läuft...");
+
+            await stepContext.Context.SendActivityAsync(Activity.CreateTypingActivity());
+
+            return await stepContext.EndDialogAsync(scheduleQuery);
         }
     }
 }
